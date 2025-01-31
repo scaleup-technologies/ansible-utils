5b45468d0a764c122e86fcd6cac4579e85db9b35
Sven Anders <sanders@scaleuptech.com>
2025-01-31 07:14:41 +0100

    Fix Doc for opnsense_facts

--- plugins/modules/opnsense_facts.py
+++ plugins/modules/opnsense_facts.py
@@ -11,30 +11,30 @@ from lxml import etree
 from ansible.module_utils.basic import AnsibleModule
 from base64 import b64encode
 from subprocess import run
+
 __metaclass__ = type
 
 
-DOCUMENTATION = r'''
+DOCUMENTATION = r"""
 ---
 module: opnsense_facts
 short_description: Get facts from opnsense
 description:
-    - Get facts form opnsense
-version_added: "1.0.0"
-options: []
+    - Get facts from opnsense.
+version_added: "1.3.0"
+options: {}
 author: Sven Anders
-'''
-EXAMPLES = r'''
+"""
+EXAMPLES = r"""
 - name: Get OpnSense Fact
   opnsense_facts:
-'''
-
-
-RETURN = r'''
+"""
+RETURN = r"""
 interfaces:
   description: Array of interfaces
   returned: success
-  type: array
+  type: list
+  elements: dict
   example:
     - descr: "MGMNetHamburg",
       enable: true,
@@ -52,19 +52,16 @@ interfaces:
       vlan_id: 409,
       vlan_parent: "ix1",
       vlan_proto: "802.1q"
-
-'''
+"""
 
 
 def recursive_dict(element):
-    return element.tag, \
-        dict(map(recursive_dict, element)) or element.text
+    return element.tag, dict(map(recursive_dict, element)) or element.text
 
 
 def main():
     module = AnsibleModule(
-        argument_spec=dict(
-        ),
+        argument_spec=dict(),
         supports_check_mode=True,
     )
 
@@ -72,9 +69,9 @@ def main():
     ansible_facts = {}
     tree = etree.parse("/conf/config.xml")
     interfaces_tree = tree.find("interfaces")
-    vip_tree = tree.find('virtualip')
+    vip_tree = tree.find("virtualip")
     # count=0
-    #context = etree.iterwalk(interfaces_tree, events=("start", "end"))
+    # context = etree.iterwalk(interfaces_tree, events=("start", "end"))
     # for action, elem in context:
     #    ansible_facts['xx_%d' % count]="%s %s" % (action, elem.tag)
     #    count=count+1
@@ -83,73 +80,82 @@ def main():
     interfaces = []
     for k in data.keys():
         interface = {}
-        interface['if'] = data[k]['if']
-        interface['descr'] = data[k]['descr']
+        interface["if"] = data[k]["if"]
+        interface["descr"] = data[k]["descr"]
 
-        if 'enable' in data[k].keys() and data[k]['enable'] == '1':
-            interface['enable'] = True
+        if "enable" in data[k].keys() and data[k]["enable"] == "1":
+            interface["enable"] = True
         else:
-            interface['false'] = True
-        interface['ip'] = []
-        if 'ipaddr' in data[k].keys():
+            interface["false"] = True
+        interface["ip"] = []
+        if "ipaddr" in data[k].keys():
             ipi = ipaddress.ip_interface(
-                "%s/%s" % (data[k]['ipaddr'], data[k]['subnet']))
-            interface['ip'].append({
-                'addr': data[k]['ipaddr'],
-                'subnet': data[k]['subnet'],
-                'network': str(ipi.network).split('/')[0],
-                'broadcast': str(ipi.network.broadcast_address),
-                'netmask': str(ipi.netmask),
-                'mode': 'if'
-            })
-        if 'ipaddrv6' in data[k].keys() and 'subnetv6' in data[k].keys():
+                "%s/%s" % (data[k]["ipaddr"], data[k]["subnet"])
+            )
+            interface["ip"].append(
+                {
+                    "addr": data[k]["ipaddr"],
+                    "subnet": data[k]["subnet"],
+                    "network": str(ipi.network).split("/")[0],
+                    "broadcast": str(ipi.network.broadcast_address),
+                    "netmask": str(ipi.netmask),
+                    "mode": "if",
+                }
+            )
+        if "ipaddrv6" in data[k].keys() and "subnetv6" in data[k].keys():
             ipi = ipaddress.ip_interface(
-                "%s/%s" % (data[k]['ipaddrv6'], data[k]['subnetv6']))
-            interface['ip'].append({
-                'addr': data[k]['ipaddrv6'],
-                'subnet': data[k]['subnetv6'],
-                'network': str(ipi.network).split('/')[0],
-                'broadcast': str(ipi.network.broadcast_address),
-                'netmask': str(ipi.netmask),
-                'mode': 'if'
-            })
-        for ele in vip_tree.findall('vip'):
+                "%s/%s" % (data[k]["ipaddrv6"], data[k]["subnetv6"])
+            )
+            interface["ip"].append(
+                {
+                    "addr": data[k]["ipaddrv6"],
+                    "subnet": data[k]["subnetv6"],
+                    "network": str(ipi.network).split("/")[0],
+                    "broadcast": str(ipi.network.broadcast_address),
+                    "netmask": str(ipi.netmask),
+                    "mode": "if",
+                }
+            )
+        for ele in vip_tree.findall("vip"):
             (tag, ce) = recursive_dict(ele)
-            if ce['interface'] == k:
+            if ce["interface"] == k:
                 ipi = ipaddress.ip_interface(
-                    "%s/%s" % (ce['subnet'], ce['subnet_bits']))
-                interface['ip'].append({
-                    'addr': ce['subnet'],
-                    'subnet': ce['subnet_bits'],
-                    'network': str(ipi.network).split('/')[0],
-                    'broadcast': str(ipi.network.broadcast_address),
-                    'netmask': str(ipi.netmask),
-                    'mode': ce['mode']
-                })
-        cmd_ifconfig = run(
-            ['/sbin/ifconfig', data[k]['if']], capture_output=True)
-        for line in cmd_ifconfig.stdout.decode('utf-8').split('\n'):
+                    "%s/%s" % (ce["subnet"], ce["subnet_bits"])
+                )
+                interface["ip"].append(
+                    {
+                        "addr": ce["subnet"],
+                        "subnet": ce["subnet_bits"],
+                        "network": str(ipi.network).split("/")[0],
+                        "broadcast": str(ipi.network.broadcast_address),
+                        "netmask": str(ipi.netmask),
+                        "mode": ce["mode"],
+                    }
+                )
+        cmd_ifconfig = run(["/sbin/ifconfig", data[k]["if"]], capture_output=True)
+        for line in cmd_ifconfig.stdout.decode("utf-8").split("\n"):
             m = re.match("\s*ether (..:..:..:..:..:..)", line)
             if m:
-                interface['mac'] = m.group(1)
+                interface["mac"] = m.group(1)
             m = re.match(
-                "\s*vlan: (\d+) vlanproto: (.*) vlanpcp: 0 parent interface: (.*)", line)
+                "\s*vlan: (\d+) vlanproto: (.*) vlanpcp: 0 parent interface: (.*)", line
+            )
             if m:
-                interface['vlan_id'] = int(m.group(1))
-                interface['vlan_proto'] = m.group(2)
-                interface['vlan_parent'] = m.group(3)
+                interface["vlan_id"] = int(m.group(1))
+                interface["vlan_proto"] = m.group(2)
+                interface["vlan_parent"] = m.group(3)
 
         interfaces.append(interface)
 
-    ansible_facts['interfaces'] = interfaces
+    ansible_facts["interfaces"] = interfaces
     c = []
-    for ele in vip_tree.findall('vip'):
+    for ele in vip_tree.findall("vip"):
         (tag, ce) = recursive_dict(ele)
         c.append(ce)
-    results['ansible_facts'] = ansible_facts
+    results["ansible_facts"] = ansible_facts
 
     module.exit_json(**results)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
